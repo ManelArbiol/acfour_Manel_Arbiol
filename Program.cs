a466109910c54abf323d76e9c1457b881cd0351f
Manel Arbiol Calpe <146890524+ManelArbiol@users.noreply.github.com>
2024-11-25 13:08:54 +0100

    Update Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+
+namespace Examen
+{
+    public class DiagramToCode
+    {
+        static void Main()
+        {
+            const string MSG_INPUT_NUMS = "Introdueix 20 números: ";
+
+            Console.WriteLine(MSG_INPUT_NUMS);            
+
+
+            int[] nums = new int[20];
+
+            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = ValidatorNum(i, "num");
+            }
+        }
+
+        static int ValidatorNum(int num, string Correct)
+        {
+            const string MSG_ERROR = "Error, introdueix un número vàlid.";
+            if (num == 0)
+            {
+                return num;
+            }
+            else
+            {
+                Console.WriteLine(MSG_ERROR);
+                return -1;
+            }
+        }
+    }
+}
